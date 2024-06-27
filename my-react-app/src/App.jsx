34a86d4c5fcd297f3d64c4a4a75397b6fddbe1bc
Marianna Mazzette <20104120@mail.wit.ie>
2024-06-27 18:17:39 +0100

    props and default props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,5 @@
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
-import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Student from './Student.jsx'
 
 function App() {
 
@@ -15,6 +13,11 @@ function App() {
       <Card />
       <Card />
       <Card />
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
+      <Student name="Larry"/>
     </>
   );
 
