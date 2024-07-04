50dfcb5915f0de99ae7ee72369060504a034aec8
Marianna Mazzette <20104120@mail.wit.ie>
2024-07-04 22:21:34 +0100

    stopwatch

---
+++ my-react-app/src/App4.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import Stopwatch from './Stopwatch.jsx';
+
+
+function App4(){
+
+  return(<>
+    <Stopwatch />
+  </>);
+}
+
+export default App4;
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
 import './index.css'
 // import App2 from './App2.jsx'
-import App3 from './App3.jsx'
+//import App3 from './App3.jsx'
+import App4 from './App4.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <App2 /> */}
-    <App3 />
+    {/* <App3 /> */}
+    <App4 />
   </React.StrictMode>,
 )
