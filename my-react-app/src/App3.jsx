c4622fccb847ec6e8dc3120f2ab42ffe95db7d33
Marianna Mazzette <20104120@mail.wit.ie>
2024-07-04 19:13:00 +0100

    useRef hook

--- my-react-app/src/App3.jsx
+++ my-react-app/src/App3.jsx
@@ -1,9 +1,13 @@
 import ComponentA from "./ComponentA.jsx";
 import React from "react";
+import MyComponent8 from "./MyComponent8.jsx";
 
 function App2(){
 
   return(<>
+    <MyComponent8 />
+    <hr></hr>
+    <hr></hr>
     <ComponentA />
   </>);
 }
